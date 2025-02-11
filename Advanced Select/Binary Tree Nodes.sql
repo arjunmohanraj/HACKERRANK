086031ecf5314b88ebf09829546261833b7806f6
Arjun Mohanraj <arjun.mohanraj.1994@gmail.com>
2025-02-11 15:20:42 +0530

    Delete Advanced Select directory

--- Advanced Select/Binary Tree Nodes.sql
+++
@@ -1,9 +0,0 @@
---Problem : https://www.hackerrank.com/challenges/binary-search-tree-1/problem
-
-select  N,
-CASE WHEN P IS NULL THEN 'Root'
-     WHEN N IN (SELECT P FROM BST) THEN 'Inner'
-     ELSE 'Leaf'
-end     
-from BST
-order by N;
